c8073fcf3504a396f6479270bc888b74adda4088
Matthew Woodard <mwoodard@honkforhelp.com>
2018-07-25 10:44:01 -0700

    Method to get coordinates of neighbor cells

--- life.rb
+++ life.rb
@@ -1,13 +1,23 @@
 class Cell
-  def neighbors
-    # Get neighbor cells
+  def neighbors(position)
+    results = []
+    # Get limits of board
+    row_limit = self.board.count
+    column_limit = self.board[0].count
+    x = [0, position[0] - 1].max
+    while x <= [position[0] + 1, row_limit].min do
+      y = [0, position[1] - 1].max
+      while y <= [position[1] + 1, column_limit].min do
+        unless [x, y] == position
+          results << [x,y]
+        end
+        y += 1
+      end
+      x += 1
+    end
   end
 
-  def position
-    # Get [x,y] position of cell
-  end
-
-  def step
+  def step(position)
     # calculate next life cycle for this cell
   end
 
